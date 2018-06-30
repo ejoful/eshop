0eb38b0be5f88463025a3b0e2cc2871e8e9f3588
ejoful <ejoful@gmail.com>
2018-06-30 22:52:01 +0800

    footer

--- run/frontend/views/layouts/main.php
+++ run/frontend/views/layouts/main.php
@@ -214,8 +214,6 @@ a {
 <footer class="footer">
     <div class="container">
         <p class="pull-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
 </footer>
 
